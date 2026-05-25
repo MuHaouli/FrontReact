865b9c55d50dfed41ce876b6f6ad9686cc5c0213
Your Name <you@example.com>
2026-05-25 15:17:45 -0300

    header with navlinks

--- my-app/src/components/headerPersonalizado.tsx
+++ my-app/src/components/headerPersonalizado.tsx
@@ -4,12 +4,11 @@ function HeaderPersonalizado(){
 
             <div>
                 <ul className="Initial-nav-list">
-                    <li>Início</li>
-                    <li>Sobre</li>
-                    <li>Contato</li>
+                    <li><a href="">Início</a></li>
+                    <li><a href="#about">Sobre</a></li>
+                    <li><a href="#contact">Contato</a></li>
                 </ul>
             </div>
-            
         </header>
     );
     

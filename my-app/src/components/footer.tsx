70c8e3c26349e1a00fc8780285e3043b792b83cf
Your Name <you@example.com>
2026-06-07 15:51:55 -0300

    fix footer

--- my-app/src/components/footer.tsx
+++ my-app/src/components/footer.tsx
@@ -1,9 +1,6 @@
 function Footer(){
     return (
         <footer>
-            <div className="ImageFooter">
-                <img src="" alt="Logo" />
-            </div>
 
             <div className="copyRightFooter">
                 <p>©Jogos Educativos, 2026. Todos os direitos reservados.</p>

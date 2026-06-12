f5f8a102f13e63981daf9e92828e153421d3f49a
MuHaouli <murilo.haouli@edu.unifil.br>
2026-06-12 10:36:34 -0300

    update maoJogador props

--- my-app/src/components/maoJogador.tsx
+++ my-app/src/components/maoJogador.tsx
@@ -1,15 +1,19 @@
-function MaoJogador() {
-  const cartas = ['A♠', '10♥']
+import type { MaoProps } from "../types/components/props.types"
 
+function MaoJogador({ cards }: MaoProps) {
   return (
     <section className="mao-jogador">
       <h3>Sua mão</h3>
       <div className="linha-cartas">
-        {cartas.map((carta) => (
-          <div key={carta} className="carta">
-            {carta}
-          </div>
-        ))}
+        {cards.length === 0 ? (
+          <div className="carta carta-vazia">Sem cartas</div>
+        ) : (
+          cards.map((carta) => (
+            <div key={carta} className="carta">
+              {carta}
+            </div>
+          ))
+        )}
       </div>
     </section>
   )

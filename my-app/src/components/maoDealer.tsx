64c1aa7cb4d517bec0a088ae5ecbafb47b5ccc36
MuHaouli <murilo.haouli@edu.unifil.br>
2026-06-12 10:36:09 -0300

    update maoDealer props

--- my-app/src/components/maoDealer.tsx
+++ my-app/src/components/maoDealer.tsx
@@ -1,15 +1,19 @@
-function MaoDealer() {
-  const cartas = ['A◆', '10♣']
+import type { MaoProps } from "../types/components/props.types"
 
+function MaoDealer({ cards }: MaoProps) {
   return (
     <section className="mao-dealer">
       <h3>Mão do Dealer</h3>
       <div className="linha-cartas">
-        {cartas.map((carta) => (
-          <div key={carta} className="carta">
-            {carta}
-          </div>
-        ))}
+        {cards.length === 0 ? (
+          <div className="carta carta-vazia">Sem cartas</div>
+        ) : (
+          cards.map((carta) => (
+            <div key={carta} className="carta">
+              {carta}
+            </div>
+          ))
+        )}
       </div>
     </section>
   )
